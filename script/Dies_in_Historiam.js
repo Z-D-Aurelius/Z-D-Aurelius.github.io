e111038cccb7822639b4483b3811f162c49c563e
Z. D. Aurelius <ulixinzov@outlook.com>
2026-04-28 12:39:30 +0800

    Gross Update

---
+++ script/Dies_in_Historiam.js
@@ -0,0 +1,17 @@
+let date = new Date();
+// let month = date.getMonth() + 1;
+// let day = date.getDate();
+let month = 1;
+let day = 1;
+
+let month_json =
+    fetch("./datum/Dies_in_Historiam/" + month + ".json").
+        then(response => response.json());
+
+month_json.then(data => {
+    day_json = data[day.toString()];
+    console.log(day_json);
+    document.getElementById("DiH_Event").innerText = day_json["event"];
+    document.getElementById("DiH_Description").innerHTML = day_json["description"];
+    document.getElementById("DiH_image").src = "./image/Dies_in_Historiam/" + month + "." + day + ".jpg";
+});
