b644ebf6c6a39f4328ba0b46755689fef67382f7
Z. D. Aurelius <ulixinzov@outlook.com>
2026-05-04 01:13:12 +0800

    Changes

--- script/Dies_in_Historiam.js
+++ script/Dies_in_Historiam.js
@@ -1,15 +1,15 @@
 let date = new Date();
-let month = date.getMonth() + 1;
-let day = date.getDate();
-// let month = 5;
-// let day = 3;
+// let month = date.getMonth() + 1;
+// let day = date.getDate();
+let month = 5;
+let day = 5;
 
 let month_json =
     fetch("./datum/Dies_in_Historiam/" + month + ".json").
         then(response => response.json());
 
 month_json.then(data => {
-    day_json = data[day.toString()];
+    const day_json = data[day.toString()];
     console.log(day_json);
     document.getElementById("DiH_Event").innerText = day_json["event"];
     document.getElementById("DiH_Description").innerHTML = day_json["description"];
